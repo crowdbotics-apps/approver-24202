edbe6cbde3842593de74667c315bd386717d9981
Crowdbotics <team@crowdbotics.com>
2021-02-02 02:49:05 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/approver_24202/MainActivity.java
@@ -0,0 +1,15 @@
+package com.approver_24202;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "approver_24202";
+    }
+}
